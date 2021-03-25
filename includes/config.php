072aead4b80943d2530fc41c438d0bbba4ce4340
Julia George <46323370+juliafaye21@users.noreply.github.com>
2021-03-25 16:48:07 -0400

    Updated files

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,30 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+//ini_set ('error_reporting', E_ALL);
+//ini_set ('display_errors','1');
+//error_reporting (E_ALL|E_STRICT);
 
+//Set variables to values for connection to database.
+$Servername = '127.0.0.1';
+$userName = 'ekbaker';
+$pass = 'X5j13$#eCM1cG@Kdc';
+$db2 = 'ecom';
+ 
+/* Attempt to connect to MySQL database */
+$con = mysqli_init();
+
+/*verify server cert is set to true*/
+mysqli_options($con,MYSQLI_OPT_SSL_VERIFY_SERVER_CERT,true);
+
+/*set the cert to be used because this is local host we are using ca cert*/   
+$con->ssl_set(NULL,NULL,'/etc/mysql/ca.pem',NULL,NULL);
+/*Uses real connect to connect to the database and forcing SSL connection on port 3306*/
+$db = mysqli_real_connect($con,$Servername,$userName,$pass,$db2,3306,NULL,MYSQLI_CLIENT_SSL);
+
+/*checks if the db is connected or not.. if it cannot connect it will die and return an error*/
+if(!$db)
+{
+    die ("Could not connect");
+}
+?>
---
+++ includes/navBar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-fixed-top" role="navigation" id="topnav">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+    		</button>
+                <a class="navbar-brand" href="#">Julia's BookStore</a>
+
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <?php 
+                    
+                   
+                    if(!isset($_SESSION['email'])){
+                        echo "<li><a href='login1.php'>Login</a></li>";
+                    }
+                    else 
+                    {
+                        echo "<li><a href='logout.php'>Logout</a></li>";
+                    }
+                     if(isset($_SESSION['email'])){
+                        $sess=$_SESSION['email'];
+                        echo "<li><a>Hi ".$_SESSION['email']." !</a></li>";
+                        echo $_SESSION['cust_id'];
+                    }
+                    else {
+                        echo "<li><a>Guest</a></li>";
+                    }
+                    ?>
+                   
+                    <li><a href="cart.php">Go to Cart<span class="badge"><?php total_items(); ?></span></a></li>
+                    
+                </ul>
+            </div>
+
+        </div>
+    </nav>
